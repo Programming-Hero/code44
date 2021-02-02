6d89b0722d45c45be92ce2c46ccd881f4794535a
Arif016210 <arifulislamwasim2017@gmail.com>
2021-02-02 21:44:23 +0600

    update all file

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCOhqBtlKkfIEb7kitPwTepxnuIrrNNJow",
+    authDomain: "burj-al-arab20.firebaseapp.com",
+    projectId: "burj-al-arab20",
+    storageBucket: "burj-al-arab20.appspot.com",
+    messagingSenderId: "558107511281",
+    appId: "1:558107511281:web:fccd1a23b6f0ea3014dc50"
+  };
+
+  export default firebaseConfig;
